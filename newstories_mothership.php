22a5a6ffca6a7c679eae54e3d939f9fb37ca6f06
xlanor <tanjingkai@me.com>
2017-08-25 11:52:55 +0800

    Added comments.

--- newstories_mothership.php
+++ newstories_mothership.php
@@ -1,12 +1,11 @@
+<?php
 /*
 Mothership scrape new articles to database script.
 Run with cronjob
 Written by xlanor.
 */
-<?php
 include('connection.php');
 include('simple_html_dom.php');
-$html = file_get_contents('https://mothership.sg/');
 $targetPage = 'https://mothership.sg/wp-admin/admin-ajax.php?action=alm_query_posts&query_type=standard&nonce=c218689620&repeater=default&theme_repeater=null&acf=&nextpage=&cta=&comments=&post_type%5B%5D=post&sticky_posts=&post_format=&category=&category__not_in=&tag=&tag__not_in=&taxonomy=&taxonomy_terms=&taxonomy_operator=&taxonomy_relation=&meta_key=&meta_value=&meta_compare=&meta_relation=&meta_type=&author=&year=&month=&day=&post_status=&order=DESC&orderby=date&post__in=&post__not_in=&exclude=&search=&custom_args=&posts_per_page=10&page=0&offset=0&preloaded=false&seo_start_page=1&paging=false&lang=&slug=home&canonical_url=https%3A%2F%2Fmothership.sg%2F';
 
 $ch = curl_init();
@@ -15,20 +14,18 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL, $targetPage);
 $result = curl_exec($ch);
 curl_close($ch);
-
 $obj = json_decode($result,true);
+
+//strips the string that we want from the jsonstring.
 $html = $obj['html'];
 $results = print_r($obj['html'],true);
 $x = new DOMDocument;
 $x->loadHTML($results);
 $clean = $x->saveXML();
-
+//cleans up the string and save it as an XML string so that we can access it later.
 $xml = new DOMDocument();
-
 $xml->loadXML($clean);
-
-$links = array();
-
+$links = array(); //creates an array to place everything that we scraped out in
 $counter = 0;
 foreach($xml->getElementsByTagName('a') as $link) 
 {
@@ -42,7 +39,7 @@ foreach($xml->getElementsByTagName('a') as $link)
 	}
 	$counter++;
 }
-
+//loops through the array that we created previously to insert into database
 foreach ($links as $link)
 {
 	$checkq = "SELECT ms_url FROM Mothership WHERE ms_url = :msurl";
